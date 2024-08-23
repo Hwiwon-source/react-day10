e4962cb51c7b09b56f76a92b22b33822bf730cf6
Hwiwon Kang <syaron9364@gmail.com>
2024-08-23 13:05:53 +0900

    bootstrap container adjustment

--- src/component/Navbar.js
+++ src/component/Navbar.js
@@ -6,6 +6,7 @@ import "./Navbar.css";
 import { faSearch } from "@fortawesome/free-solid-svg-icons";
 import Menu from "./Menu";
 import { useNavigate } from "react-router-dom";
+import { Container } from "react-bootstrap";
 
 const Navbar = ({ authenticate, setAuthenticate }) => {
   const navigate = useNavigate();
@@ -26,31 +27,29 @@ const Navbar = ({ authenticate, setAuthenticate }) => {
   };
 
   return (
-    <div className="Navbar">
-      <div className="Navbar-wrap">
-        <div>
-          <div className="login-area" onClick={handleLogin}>
-            <FontAwesomeIcon icon={faUser} />
-            <div>{authenticate ? "로그아웃" : "로그인"}</div>
-          </div>
-        </div>
-        <div className="logo-section">
-          <img
-            onClick={goToHome}
-            width="100px"
-            src="https://static.vecteezy.com/system/resources/previews/023/871/762/non_2x/hm-brand-logo-symbol-black-design-hennes-and-mauritz-clothes-fashion-illustration-free-vector.jpg"
-            alt="h&mLogo"
-          />
-        </div>
-        <div className="menu-area">
-          <Menu />
-          <div className="search-box m-0">
-            <FontAwesomeIcon icon={faSearch} />
-            <input className="search-input" type="text" placeholder="드레스" />
-          </div>
+    <Container className="Navbar">
+      <div>
+        <div className="login-area" onClick={handleLogin}>
+          <FontAwesomeIcon icon={faUser} />
+          <div>{authenticate ? "로그아웃" : "로그인"}</div>
         </div>
       </div>
-    </div>
+      <div className="logo-section">
+        <img
+          onClick={goToHome}
+          width="100px"
+          src="https://static.vecteezy.com/system/resources/previews/023/871/762/non_2x/hm-brand-logo-symbol-black-design-hennes-and-mauritz-clothes-fashion-illustration-free-vector.jpg"
+          alt="h&mLogo"
+        />
+      </div>
+      <div className="menu-area">
+        <Menu />
+        <div className="search-box m-0">
+          <FontAwesomeIcon icon={faSearch} />
+          <input className="search-input" type="text" placeholder="드레스" />
+        </div>
+      </div>
+    </Container>
   );
 };
 
